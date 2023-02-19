508a7d5e67ec83853d12e8eb59f2d1beca367809
Hamidouuuu <125186832+Hamidouuuu@users.noreply.github.com>
2023-02-19 17:28:27 +0100

    Delete weekendsVerified.js

--- weekendsVerified.js
+++
@@ -1,7 +0,0 @@
-var jour = prompt("Entrer un jour: ");
-    var weekends = 'samedi'|| 'dimanche';
-    if (jour == weekends) {
-        console.log('weekend');
-    } else {
-        console.log('weekday');
-    }
